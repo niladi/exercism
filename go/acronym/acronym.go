7b1a5a4d70d549404e650720ccf449ac6d90dd9e
Niklas Lakner <niklas.lakner@web.de>
2020-03-29 14:26:53 +0200

    go acronym finished

---
+++ go/acronym/acronym.go
@@ -0,0 +1,27 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package acronym should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package acronym
+
+import (
+	"bytes"
+	"regexp"
+	"strings"
+)
+
+// Abbreviate should have a comment documenting it.
+func Abbreviate(s string) string {
+	// Write some code here to pass the test suite.
+	// Then remove all the stock comments.
+	// They're here to help you get started but they only clutter a finished solution.
+	// If you leave them in, reviewers may protest!
+	re := regexp.MustCompile("([^A-Z'][A-Z]|^[A-Z])")
+	letters := re.FindAllString(strings.ToUpper(s), -1)
+	var b bytes.Buffer
+	for _, v := range letters {
+		b.WriteByte(v[len(v)-1])
+	}
+	return b.String()
+}
